ec2c5cdadf9d6eccd16bda5ba9ab339b97b87466
Prashanthi <rvprashanthi@gmail.com>
2016-09-24 16:15:20 +0530

    Multitenancy

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,12 @@ Route::group(['domain'=>'{subdomain}.newsletters.imm9ntup5j.us-east-1.elasticbea
      * test
      */
 
-    Route::get('test', function () {
-        if(\DB::connection()->getDatabaseName())
-        {
-            echo "Connected sucessfully to database ".\DB::connection()->getDatabaseName().".";
-        }
+    Route::get('test', function ($subdomain) {
+//        if(\DB::connection()->getDatabaseName())
+//        {
+//            echo "Connected sucessfully to database ".\DB::connection()->getDatabaseName().".";
+//        }
+        return $subdomain;
 
     });
 
