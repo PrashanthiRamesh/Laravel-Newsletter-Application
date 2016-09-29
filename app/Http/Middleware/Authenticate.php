cd6dd4221e8f47111f0d24842d5383ead19180ff
Prashanthi <rvprashanthi@gmail.com>
2016-09-29 13:19:33 +0530

    Beanstalk commit

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -21,15 +21,20 @@ class Authenticate
     public function handle($request, Closure $next)
     {
 
+        $pieces = explode('.', $request->getHost());
+        $username = User::where('username', '=', $pieces[0])->first();
 
         Config::set('database.default', 'mysql');
         DB::reconnect();
 
-        if (!Auth::check()) {
+        if (!Auth::check()  ) {
             return Redirect::to('login');
         }else{
             $user_id=Auth::user()->id;
             $user= User::where('id', $user_id)->first();
+            if($username!=$user->username){
+                return Redirect::to('login');
+            }
             Config::set('database.connections.mysql_tenant.database', $user->username);
             Config::set('database.default', 'mysql_tenant');
             DB::reconnect('mysql_tenant');
